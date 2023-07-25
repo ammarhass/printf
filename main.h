c22e2c9d959d98f4f1915b1e4645c63591cad52b
ahmed726796 <ahmedhassan726796@gmail.com>
2023-07-25 09:31:57 -0700

    updated main.h file

--- main.h
+++ main.h
@@ -3,24 +3,28 @@
 
 #include<stdio.h>
 #include<unistd.h>
+#include<stdarg.h>
+#include<string.h>
+
 /**
- * struct print - structure contains pointer to function 
+ * struct print - structure contains pointer to function
  * that takes a va_list and return int and a char
  * this structure used to handle print functions
  *
- * @p - pointer to function
- * @c - char to choose print functions
+ * @p: pointer to function
+ * @c: char to choose print functions
  */
 
 typedef struct print
 {
-        int (* p)(va_list);
-        char c;
-}print_st;
+	int (*p)(va_list);
+	char c;
+} print_st;
 
+int (*choose_print(char ch))(va_list);
 int _printf(const char *format, ...);
-int p_char(va_list p);
-int print_string(va_list p);
+int P_char(va_list p);
+int P_string(va_list p);
 int _putchar(char c);
 void print_num(int num);
 
